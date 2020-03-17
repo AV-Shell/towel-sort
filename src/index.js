994d0b1422a235c13404fa850d4f61afddcf63c8
AV-Shell <avshell.sj@gmail.com>
2020-03-17 20:03:23 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!(Array.isArray(matrix) && (matrix.length >0))){
+    return [];
+  }
+  let newmass = matrix.reduce( (a,b,index) => {
+    if ((index%2) != 1){
+      return a.concat(b);
+    } else {
+      return a.concat(b.reverse());
+    }
+ 
+  },[]);
+  return newmass;
 }
